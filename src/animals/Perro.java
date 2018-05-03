d4151842061e403f46b6e4d3f4f253df3974bc2c
user <user@user-PC>
2018-05-03 10:18:35 +0200

    Modifiquem el metode sonido dins de Perro.java, i canviem GUAU per BUPBUP

--- src/animals/Perro.java
+++ src/animals/Perro.java
@@ -8,6 +8,6 @@ public class Perro extends Animal {
     // ----------------------------
     @Override
     public void sonido() {
-        System.out.println(this.getNombre() + " dice: GUAU !");
+        System.out.println(this.getNombre() + " dice: BUPBUP !");
     }
 }
