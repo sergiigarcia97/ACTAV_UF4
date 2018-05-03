9574409f47e618be6e0353c1ece32c1b672625ea
user <user@user-PC>
2018-05-03 10:17:07 +0200

    Afegim el mètode correr que mostra el nom de l'animal i que esta corrent

---
+++ src/animals/Animal.java
@@ -0,0 +1,31 @@
+package animals;
+
+public abstract class Animal {
+
+    private String nombre;
+
+    public Animal() {
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void correr(){
+         System.out.println(this.getNombre() + " está corriendo...");
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void comer() {
+        System.out.println(this.getNombre() + " está comiendo...");
+    }
+
+    public void caminar() {
+        System.out.println(this.getNombre() + " está caminando...");
+    }
+
+    public abstract void sonido();
+}
